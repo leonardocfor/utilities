0ab33b88618a053067d1e1af7b1f7a2d279fb54c
leonardocfor <lecf.77@gmail.com>
2019-08-06 22:40:44 -0500

    Region filterer V.1.2. It eliminates sea points

--- biology/region_filterer.py
+++ biology/region_filterer.py
@@ -8,6 +8,7 @@ It also eliminates repeated locations
 
 import optparse
 import os
+from mpl_toolkits.basemap import Basemap
 
 
 ### Global variables
@@ -26,9 +27,9 @@ def filter(lat,lon):
     if lon.count('.') > 1: lon = fix_values(lon)
     lat = float(lat)
     lon = float(lon)
+    land_point = bm.is_land(lon,lat)
     include_it = False
-    if lat > MIN_LAT and lat < MAX_LAT and lon > MIN_LON and lon < MAX_LON:
-        include_it = True
+    if lat > MIN_LAT and lat < MAX_LAT and lon > MIN_LON and lon < MAX_LON and land_point: include_it = True
     return include_it
 
 def fix_values(value_to_fix):
@@ -46,24 +47,26 @@ def fix_values(value_to_fix):
                 skip_dots = True
     return correct_value
 
-def remove_repeated_elements():
+def remove_ocean_points():
 
     """
-    Remove repeated locations per species
+    Removing points in the sea
     """
     global species_dict
     species_dict_copy = {}
+
     for species in species_dict:
         species_dict_copy[species] = []
         for pair in species_dict[species]:
-            if pair not in species_dict_copy[species]:
+            if bm.is_land(float(pair[0]),float(pair[1])):
                 species_dict_copy[species].append(pair)
 
     species_dict = species_dict_copy
 
 def main():
 
-    global species_dict
+    global species_dict, bm
+    bm = Basemap(area_thresh=0)
     parser = optparse.OptionParser()
     parser.add_option('-f','--folder',dest='folder',help='Folder with CSV files')
     #parser.add_option('-m','--mac',dest='new_mac',help='New MAC address')
@@ -87,8 +90,11 @@ def main():
 
     for file in files:
         count = 0
+        print('--------------------------------------------------------')
         print('Processing species file: '+file.split('/')[-1])
         print('--------------------------------------------------------')
+        filtered_because_box = 0
+        repeated_elements = 0
         for line in open(file):
             if count > 0:
                 data = line.strip().split(';')
@@ -98,16 +104,25 @@ def main():
                 include_it = filter(latitude,longitude)
                 if include_it:
                     if species not in species_dict: species_dict[species] = []
-                    species_dict[species].append([latitude,longitude])
+                    pair = [latitude,longitude]
+                    if pair not in species_dict[species]: species_dict[species].append(pair)
+                    else:  repeated_elements +=1
+                else: filtered_because_box +=1
             else: header =   line.strip()
             count+=1
         headers[species] = header
+        if species in  species_dict: print('[+] Quantity of kept points: '+str(len(species_dict[species])))
+        else: print('[-] All points related to species were removed')
+        print('[-] Quantity of filtered points because of box or in the sea: '+str(filtered_because_box))
+        print('[-] Quantity of repeated points: '+str(repeated_elements))
 
-    remove_repeated_elements()
-
+    #remove_repeated_elements()
+    #remove_ocean_points()
     filtered_folder = folder[0:-1]+'_filtered'
     os.mkdir(filtered_folder)
+    print('########################################################')
     print('Output folder: '+filtered_folder)
+    print('########################################################')
 
 
     for species in species_dict:
